b0cae043e936e89a9909a01ce6f4ae21a021be33
Jack Christopher <jhuaihuah@unsa.edu.pe>
2024-02-10 11:48:06 -0500

    feat: get func/op list for each parameter

--- ast_parser.py
+++ ast_parser.py
@@ -12,12 +12,6 @@ def traverse(node, arg, indent=0):
     children = ast.iter_child_nodes(node)
     if children:
         for child in children:
-            
-            # if isinstance(child, ast.Name):
-            #     print('[CH] ' + '  ' * indent + str(child) + " <" + child.id + ">", arg==child.id, node)
-            # else:
-            #     print('[CH] ' + '  ' * indent + str(child))
-                
             if isinstance(child, ast.Name) and child.id == arg:
                 # print("found arg: ", child.id, " in ", node)
                 content.append(node)
@@ -55,48 +49,48 @@ def analyze(func):
     # annotations = func.__annotations__
     
     args = list(inspect.signature(func).parameters.keys())
-    print("args:", args)
+    # print("args:", args)
 
     result = {}
     for arg in args:
-        # print("\n[ARG] ", arg, "<"+(type(arg)).__name__+">")
         content = func_parser(func, arg)
         content = sanitize(content)
         temp = []
         for node in content:
             sub_content = {}
-            # print("  [NODE] ", "<"+(type(node)).__name__+">")
             for k, v in node.__dict__.items():
                 if k not in ["lineno", "col_offset", "end_lineno", "end_col_offset"]:
-                    # print("    ", k, "<"+(type(v)).__name__+">:", (v.id if isinstance(v, ast.Name) else v))
-                    sub_content[k] = [type(v).__name__, v]
+                    # sub_content[k] = [type(v).__name__, v]
+                    # if type(v).__name__ == 'list' iterate over them and capture the type of every item inside
+                    if type(v).__name__ == 'list':
+                        sub_content[k] = []
+                        for item in v:
+                            sub_content[k].append([type(item).__name__, item])
+                    else:
+                        sub_content[k] = [type(v).__name__, v]
 
             sub_content['type'] = type(node).__name__
             temp.append(sub_content)
             # print()
         result[arg] = temp
 
-    pprint.pprint(result)
+    # pprint.pprint(result)
 
 
     return result
 
 
-
-        
-
-
-
-
-
-from data.arithmetics import gcd
-from data.is_odd import is_odd
-from data.sum import add_two_numbers
-from data.foo import bar
-
-# analyze(add_two_numbers)
-# analyze(gcd)
-# res = analyze(is_odd)
-res = analyze(bar)
-
-# print(res)
+def process_result(result):
+    temp = {}
+    for k, v in result.items():
+        temp[k] = []
+        for item in v:
+            if 'func' in item.keys() and item['type'] == 'Call':
+                temp[k].append(item['func'][1].id)
+            elif 'op' in item.keys():
+                temp[k].append(item['op'][0])
+            elif 'ops' in item.keys():
+                # it's a list of operations, so we need to extract the names
+                temp[k] += [op[0] for op in item['ops']]
+    
+    return temp
